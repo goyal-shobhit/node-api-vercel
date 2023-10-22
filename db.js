039cf50c23370ed7ba447b8ced89a8fa726fb69a
shobhit goyal <shobhitgoyal46@gmail.com>
2023-10-22 18:09:52 +0530

    fifth commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/docketdatabase', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/docketdatabase', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
